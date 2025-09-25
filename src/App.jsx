859d278801d3cb0bc4236fc0ba8debf0647d9998
MiqdadAdmin <professor12001@gmail.com.com>
2025-09-25 22:16:25 +0300

    debug: Print env variable to screen

--- src/App.jsx
+++ src/App.jsx
@@ -58,9 +58,11 @@ function App() {
     };
     fetchContent();
   }, []);
-
   return (
+    
     <div className="App">
+      <h1>API URL IS: {import.meta.env.VITE_API_URL}</h1>
+
       <Header />
       <main>
         <Routes>
